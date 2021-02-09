8dc9cf25e8467436a9b9da73d050f588c7de865e
Vishal643 <vishalkumar199812@gmail.com>
2021-02-09 16:26:18 +0530

    changes index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 3000;
 const command = `json-server --watch db.json --port ${port}`;
 
 exec(command, (err, stdout, stderr) => {
-  if (err) {
-    console.log('Error running exec', err);
-    return;
-  }
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+	if (err) {
+		console.log('Error running exec', err);
+		return;
+	}
+	console.log('stdout:', stdout);
+	console.log('stderr:', stderr);
 });
